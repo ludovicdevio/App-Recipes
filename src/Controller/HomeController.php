7fa840eb09b47f1f44c99e8574364b7da9f95e37
Ludovic Dev <ludovicdev.io@gmail.com>
2024-10-10 13:02:04 +0200

    Fonctionnement du routing et de l'Object Request

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController{
+    #[Route('/', name: 'home')]
+    function index(Request $request):Response{
+        dd($request);
+        return new Response('Bonjour ' .$request->query->get('name', 'inconnu'));
+    }
+}
