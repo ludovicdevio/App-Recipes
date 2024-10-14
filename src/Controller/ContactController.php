ba8c1a7a4e90a28b217d22052393279cf50ece00
Ludovic Dev <ludovicdev.io@gmail.com>
2024-10-14 18:57:08 +0200

    Mise en place de l'envoi des emails avec Mailpit

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\DTO\ContactDTO;
+use App\Form\ContactType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request, MailerInterface $mailer): Response
+    {
+        $data = new ContactDTO();
+
+        $form = $this->createForm(ContactType::class, $data);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            try {
+                $mail = (new TemplatedEmail())
+                    ->to($data->service)
+                    ->from($data->email)
+                    ->subject('Demande de contact')
+                    ->htmlTemplate('emails/contact.html.twig')
+                    ->context(['data' => $data]);
+                $mailer->send($mail);
+                $this->addFlash('success', 'Votre email a bien été envoyé');
+                return $this->redirectToRoute('contact');
+            } catch (\Exception $e) {
+                $this->addFlash('danger', 'Impossible d\'envoyer votre email');
+            }
+        }
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
---
+++ src/DTO/ContactDTO.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\DTO;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ContactDTO
+{
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 200)]
+    public string $name = '';
+
+    #[Assert\NotBlank]
+    #[Assert\Email]
+    public string $email = '';
+
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3, max: 200)]
+    public string $message = '';
+
+    #[Assert\NotBlank]
+    public string $service = '';
+}
